7d86493e8b2e9c36130e3a3252bfe28b0c25bbbf
EmersonRoncancio <roncancioavila@gmail.com>
2026-02-01 02:13:06 -0500

    Update Technologies component to include URLs for each technology

--- src/app/home/components/technologies/technologies.ts
+++ src/app/home/components/technologies/technologies.ts
@@ -8,21 +8,23 @@ import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 })
 export class Technologies {
 
-  technologies = signal<{ name: string; category: string }[]>([
-    { name: "React", category: "Frontend" },
-    { name: "Next.js", category: "Framework" },
-    { name: "TypeScript", category: "Language" },
-    { name: "Node.js", category: "Backend" },
-    { name: "Java", category: "Backend" },
-    { name: "Spring Boot", category: "Backend" },
-    { name: "PostgreSQL", category: "Database" },
-    { name: "Docker", category: "DevOps" },
-    { name: "Kubernetes", category: "DevOps" },
-    { name: "NestJs", category: "Backend" },
-    { name: "Angular", category: "Frontend" },
-    { name: "Git", category: "Version Control" },
-    { name: "MySQL", category: "Database" },
-    { name: "MongoDB", category: "Database" },
-    { name: "WordPress", category: "CMS" },
+  technologies = signal<{ name: string; category: string; url: string  }[]>([
+    { name: "React", category: "Frontend", url:"https://es.react.dev/" },
+    { name: "Next.js", category: "Framework", url:"https://nextjs.org/" },
+    { name: "TypeScript", category: "Language", url:"https://www.typescriptlang.org/" },
+    { name: "Node.js", category: "Backend", url:"https://nodejs.org/en" },
+    { name: "Java", category: "Backend", url:"https://www.java.com/es/" },
+    { name: "Spring Boot", category: "Backend", url:"https://spring.io/" },
+    { name: "PostgreSQL", category: "Database", url:"https://www.postgresql.org/" },
+    { name: "Docker", category: "DevOps", url:"https://www.docker.com/" },
+    { name: "Kubernetes", category: "DevOps", url:"https://kubernetes.io/es/" },
+    { name: "NestJs", category: "Backend", url:"https://nestjs.com/" },
+    { name: "Angular", category: "Frontend", url:"https://angular.io/" },
+    { name: "Git", category: "Version Control", url:"https://git-scm.com/" },
+    { name: "MySQL", category: "Database", url:"https://www.mysql.com/" },
+    { name: "MongoDB", category: "Database", url:"https://www.mongodb.com/" },
+    { name: "WordPress", category: "CMS", url:"https://wordpress.org/" },
+    { name: "n8n", category: "Automation", url:"https://n8n.io/" },
+    { name: "make.com", category: "Automation", url:"https://www.make.com/" },
   ])
 }
