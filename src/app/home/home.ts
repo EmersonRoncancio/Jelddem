98efb22d17d5f33bc093b5c5f7a2fc2775794ae1
LuiferEduardoo <luifer01ortegaperez@gmail.com>
2026-03-12 18:47:24 -0500

    feat: Add section Trust

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -3,13 +3,14 @@ import { Hero } from "./components/hero/hero";
 import { Services } from "./components/services/services";
 import { Projects } from "./components/projects/projects";
 import { About } from "./components/about/about";
+import { Trust } from "./components/trust/trust";
 import { Cta } from "./components/cta/cta";
 import { Footer } from "./components/Footer/Footer";
 import { WhatsApp } from "./components/whatsapp/whatsapp";
 
 @Component({
   selector: 'app-home',
-  imports: [Hero, Services, Projects, About, Cta, Footer, WhatsApp],
+  imports: [Hero, Services, Projects, About, Trust, Cta, Footer, WhatsApp],
   templateUrl: './home.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
